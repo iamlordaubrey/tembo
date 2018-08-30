15e94e97af04aa45ed72fa857c646b5943792043
Chiemeka Alim <lord.chiemy@gmail.com>
2018-08-30 02:14:14 +0100

    User can add new parents

--- solution.py
+++ solution.py
@@ -1,37 +1,40 @@
 from CodeChallengeJune18 import parents, activities
 
 
-def nice_print(data):
+def nice_print(message, data=None):
     """
     Print a well formatted report
+    :param message: A message string to print to the terminal
     :param data: A list of dictionaries
     :return: The main message
     """
-    main_message = ''
+    if message is not '':
+        print(message)
+        return message
 
     for item in data:
-        message = '\n'
-        message += f'Hi {item.get("parent_name")}\n'
+        temp_message = '\n'
+        temp_message += f'Hi {item.get("parent_name")}\n'
 
         list_activities = '\n'.join(item.get('activities'))
 
         if not item.get('child_name'):
-            message += 'Sorry, no child name found!\n\n'
+            temp_message += 'Sorry, no child name found!\n\n'
         elif not item.get('age'):
-            message += f'Sorry, no age found for {item.get("child_name")}!\n\n'
+            temp_message += f'Sorry, no age found for {item.get("child_name")}!\n\n'
         elif not list_activities:
-            message += f'Sorry, no activity for {item.get("child_name")}!\n\n'
+            temp_message += f'Sorry, no activity for {item.get("child_name")}!\n\n'
         else:
-            message += f'Here\'s a list of activities for {item.get("child_name")}:\n\n'
-            message += f'{list_activities}\n\n'
+            temp_message += f'Here\'s a list of activities for {item.get("child_name")}:\n\n'
+            temp_message += f'{list_activities}\n\n'
 
-        message += 'Curriculum complete!\n'
-        message += '\n'
+        temp_message += 'Curriculum complete!\n'
+        temp_message += '\n'
 
-        main_message += message
+        message += temp_message
 
-    print(main_message)
-    return main_message
+    print(message)
+    return message
 
 
 def get_activities(parents_dict, activities_list):
@@ -64,9 +67,82 @@ def get_activities(parents_dict, activities_list):
     return result
 
 
+def check_binary(user_input):
+    try:
+        integer_input = int(user_input)
+        if integer_input in [0, 1]:
+            return True
+
+    except ValueError:
+        pass
+
+    return False
+
+
+def check_validity(user_input, type_fn=int):
+    try:
+        if type_fn is int:
+            return int(user_input) > 0
+
+        if type_fn(user_input):
+            return True
+
+        return False
+
+    except ValueError:
+        return False
+
+
+def prompter(message, error, type_fn):
+    user_input = input(message)
+    is_valid_integer = check_validity(user_input, type_fn)
+
+    if not is_valid_integer:
+        nice_print(error)
+        return prompter(message, error, type_fn)
+
+    if type_fn == check_binary and int(user_input) == 0:
+        return False
+
+    return user_input
+
+
+def add_parent(parents_dict):
+    opt_in = 'Would you like to add a parent?\n'
+    opt_in += 'Enter 1 for Yes, or 0 for No: '
+    opt_in_error = 'Value should either be 0 or 1\n'
+
+    # User enters 0
+    if not prompter(opt_in, opt_in_error, type_fn=check_binary):
+        return
+
+    parent_name = input('Please enter a parent name: ')
+    child_name = input (f'Please enter {parent_name}\'s child\'s name: ')
+
+    # Validate child's age
+    age_message = f'Please enter {child_name}\'s age: '
+    age_error = 'Value should be a positive integer.\n'
+    child_age = prompter(age_message, age_error, type_fn=int)
+
+    parents_dict[parent_name] = {
+        'childName': child_name,
+        'age': int(child_age)
+    }
+    nice_print(parents_dict)
+
+    # Add another parent...
+    add_parent(parents_dict)
+
+
 def sentry():
+    # User add's a parent
+    add_parent(parents)
+
+    # Get all the activities per child
     child_activities = get_activities(parents, activities)
-    nice_print(child_activities)
+
+    # Print nicely formatted info to the terminal
+    nice_print('', data=child_activities)
 
 
 if __name__ == '__main__':
